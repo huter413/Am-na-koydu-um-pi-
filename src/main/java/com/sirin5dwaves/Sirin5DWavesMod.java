3c6fad5eb25084179a3034b75f121dd08376ec8c
huter413 <musab.hira2@gmail.com>
2026-09-20 13:19:31 +0300

    Add src/main/java/com/sirin5dwaves/Sirin5DWavesMod.java

---
+++ src/main/java/com/sirin5dwaves/Sirin5DWavesMod.java
@@ -0,0 +1,9 @@
+package com.sirin5dwaves;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Sirin5DWavesMod.MODID)
+public final class Sirin5DWavesMod {
+    public static final String MODID = "sirin5dwaves";
+    public Sirin5DWavesMod() { }
+}
